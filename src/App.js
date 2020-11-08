ced3d48a94aa53bc5a7ce3faf44485c9c0c36465
JSKookoo <57160824+JSKookoo@users.noreply.github.com>
2020-11-08 12:50:02 +0900

    최종 완성 코드

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './routes/Home';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Navigation from './component/Navigation';
+import Detail from './routes/Detail';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About}/>
+      <Route path="/movie-detail" component= {Detail} />
+    </HashRouter>
   );
 }
 
 export default App;
+
+// import React from "react"; // 이 코드가 있어야 리액트가 JSX를 이해할 수 있다.
+// import axios from 'axios';
+// import Movie from './Movie';
+// import './App.css';
+
+// class App extends React.Component{
+//   state = {
+//     isLoading: true,
+//     movies:[],
+//   };
+  
+//   getMovies = async () => {
+//     const {
+//       data: {
+//         data: { movies },
+//       },
+//     }  = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+//     this.setState({movies, isLoading: false})
+//   }
+  
+//   componentDidMount(){
+//     // 영화 데이터 로딩!
+//     this.getMovies();
+//   }
+
+//   render(){
+//     const { isLoading, movies} = this.state;
+//     return (<section className="container"> 
+//           {isLoading ? (
+//             <div className="loader">
+//               <span className="loader__text">Loading...</span>
+//             </div>
+//           ) : (<div className="movies">
+//            {movies.map(movie=> (
+//             <Movie 
+//                   key = {movie.id}
+//                   id= {movie.id}
+//                   year= {movie.year}
+//                   title= {movie.title}
+//                   summary= {movie.summary}
+//                   poster= {movie.medium_cover_image}
+//                   genres= {movie.genres}
+//             />
+//            ))}
+//            </div>)}
+//     </section>
+//     );
+//   }
+// }
+
+// export default App; // 다른 파일에서 Potato 컴포넌트 사용가능하게함
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"; // 이 코드가 있어야 리액트가 JSX를 이해할 수 있다.
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root")); // App 컴포넌트가 id가 root인 엘리먼트에 그려진다.
+// 리액트는 <App />과 같은 표시를 컴포넌트로 인식함
